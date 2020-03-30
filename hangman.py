8ae3d612aa894015b75789dac0d1983194491176
themugglehacker <36447410+themugglehacker@users.noreply.github.com>
2020-03-30 12:25:47 +0530

    update hangman.py

--- hangman.py
+++ hangman.py
@@ -4,22 +4,16 @@ import random
 
 words = 'BATMAN'
 user = None
-turn = None
+turn = 6
 your_word = '_ ' * len(words)
 your_guess = your_word[0:len(your_word)-1]
 list1 = your_guess.split()
 
-while turn == None:
-    try:
-        turn = int(input('Enter the number of turn you want to guess the word:'))
-        if turn < len(words):
-            print('Your turn must larger or equal to word length')
-            turn = None
-    except ValueError:
-        print('Please enter valid value')
-        turn = None
 
 while user == None:
+    print(your_word)
+    print(your_guess)
+    print(list1)
     user = input('Enter the character you guess:').upper()
     if len(user) != 1:
         print('Please enter one character')
@@ -29,6 +23,9 @@ while user == None:
             list1[i] = user
     your_guess = ''.join(list1)
     print('Your guess is: {}'.format(your_guess))
+    if your_guess==words:
+      print("You win")
+      break
     turn -= 1
     print('You have {} chances left to guess'.format(turn))
     if turn == 0:
